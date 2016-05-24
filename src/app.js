28f2411136cf691ab19b734696679e7911e493c9
Gijs van Til <gijsvantil@gmail.com>
2016-05-24 14:25:47 +0200

    app works

--- src/app.js
+++ src/app.js
@@ -1,11 +1,16 @@
 var pg = require ('pg');
 
 var connectionString = "postgres://pgadmin:pwdaccess@localhost/gijsvantil"
+
 pg.connect(connectionString, function (error, client, done){
-	client.query('select * from hats', function (error, result){
+	var user = process.argv[2]
+	client.query('select name from hats where user_id = (select id from users where name = $1)', [user], function (error, result){
+		
+		console.log (user)
 		console.log (result.rows);
 
 		done()
 		pg.end()
 	})
 })
+
